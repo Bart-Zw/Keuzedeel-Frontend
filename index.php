f9c99fba7c32de883647eb86175d25c01bb686cc
Bart-Zw <9007171@student.zadkine.nl>
2022-04-01 10:23:34 +0200

    1-4-2022

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl"> 
+<head>
+	 <meta charset="UTF-8">
+	 <title>Portfolio</title>
+	 <link rel="stylesheet" type="text/css" href="company.css">  
+</head>
+
+<body>  
+	<header>
+		<!-- hieronder wordt het menu opgehaald. -->
+		<?php
+			include "nav.html";
+		?>
